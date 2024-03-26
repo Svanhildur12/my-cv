60afb5e4af961f59a844501c79a39873e3b5c981
Svanhildur12 <svanhildur12@gmail.com>
2024-03-26 22:38:03 +0000

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,79 @@
-import Image from "next/image";
+'use client'
+import React from "react"
+import MyImage from "./IMG_1344.jpeg"
+
+
+const DATA = [
+  {
+    id:0,
+    title:'Cashier',
+    subTitle:'June 2006 - December 2010',
+    describtion:
+    'Worked at 11-11 wich later became Kjarval. Did mostly cashier work but also alot of stocking shelves.Evantually I became a shift-manager with the resposibility of opening and closing the store.'
+  },
+  {
+    id:1,
+    title:'Food Industry',
+    subTitle:'January 2011 - May 2012',
+    describtion:'Worked at SS.First I was making the 1944 dinners but quickly took over the department of special foods.Where I made food for people with allergies or people in the hospital.',
+  },
+  {
+    id:2,
+    title:'CNC Milling Specialist',
+    subTitle:'September 2012 - September 2022',
+    describtion:'Worked at Össur HQ. I was a CNC operator mostly working on the milling machines.Later on I took on the resposibility to become the safety officer of the CNC department. And I did that job along with being a CNC operator.I left my job due to sudden illness.', 
+  },
+]
+
+
+type ImageProps={
+}
+
+const Image = (props: ImageProps) => {
+  return (
+    <div className="flex justify-end">
+      <img src={MyImage.src} alt="Picture" className="p-2 w-28 lg:w-40 h-28 lg:h-40 object-cover rounded-full overflow-hidden"/>
+    </div>
+  )
+}
+
+type HeaderProps={
+ 
+}
+
+const Header = (props: HeaderProps) => {
+  return(
+    <div>
+      <Image/>
+      <h1 className='pl-2 pt-2 text-2xl md:text-5xl font-bold font-cursive'>Svanhildur Ragna Guðmundsdóttir</h1>
+      <p className="m-2 text-sm md:text-lg text-gray-500 font-cursive">Studying Programming at NTV skólinn and full time mom</p>
+      <hr className='border-2 border-black'></hr>
+    </div>
+)}
+
+type ContentProps = {
+  data: typeof DATA
+}
+
+const Content = (props: ContentProps) => {
+  return(
+     <div>
+      {props.data.map(item => (
+        <>
+        <div className='ml-2 text-lg md:text-4xl font-bold text-left mt-10' key={item.id}>{item.title}</div>
+        <div className='ml-2  text-gray-500 font-semibold text-sm md:text-lg' key={item.id}>{item.subTitle}</div>
+        <div className="ml-2 mr-2 mt-1 text-base md:text-2xl" key={item.id}>{item.describtion}</div>
+        </>
+      ))}
+     </div>
+)
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <div className="bg-amber-50 min-h-screen">
+      <Header/>
+      <Content data={DATA}/>
+    </div>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
@@ -12,9 +13,14 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        },
+        fontFamily: {
+          Montserrat: ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+          cursive: ['cursive']
+        }
+      }
     },
-  },
   plugins: [],
 };
+
 export default config;
